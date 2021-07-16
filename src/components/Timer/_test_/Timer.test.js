8f5da79aeab7a8c45042915fcaafc17d8964df39
NataliiaBorshch <you@example.com>
2021-07-16 09:59:53 +0300

    timer test

--- src/components/Timer/_test_/Timer.test.js
+++ src/components/Timer/_test_/Timer.test.js
@@ -2,21 +2,12 @@ import * as React from 'react';
 import { mount, shallow, unmount } from 'enzyme';
 import { Timer } from '../Timer';
 
-describe('Timer Utin Test ', () => {
+describe('Timer Utin Test', () => {
   let wrapper;
-  it('Timer mount ', () => {
+  it('Timer mount', () => {
     wrapper = shallow(<Timer />);
     jest.useFakeTimers();
     expect(wrapper.length).toBe(1);
     expect(wrapper.state('time')).toEqual(new Date().toLocaleTimeString());
-  });
-  it.skip('Timer  update after 1000 ', () => {
-    wrapper = shallow(<Timer />);
-    jest.useFakeTimers();
-    wrapper.debug();
-    jest.advanceTimersByTime(1000);
-    wrapper.update();
-    console.log(wrapper.debug());
-    // expect(wrapper.state('time')).toEqual();
   });
 });
