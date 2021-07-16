c0296ec8718d4dc991ea6c01858d6a491e06ce8a
NataliiaBorshch <you@example.com>
2021-07-16 13:48:54 +0300

    usermenu fix styles

--- src/components/AppBar/UserMenu/UserMenu.jsx
+++ src/components/AppBar/UserMenu/UserMenu.jsx
@@ -1,27 +1,26 @@
 import { connect } from 'react-redux';
 import './UserMenu.scss';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import userSelectors from '../../../redux/users/select-use';
 import userOperation from '../../../redux/users/operations-user';
 
 const UserMenu = ({ name, onLogout }) => {
   return (
-    <div id="usermenu">
-      <img
-        alt="avatar"
-        width="40px"
-        height="auto"
-        src="https://ik.imagekit.io/s2fpg15d4rx/kisspng-computer-icons-portable-network-graphics-user-icon-my-b-tab-svg-png-icon-free-download-8-584-onl-5bfa37607f3959.2067798415431248325211_-Xj5AVLFlyoz.png"
-      />
+    <div id="user_menu">
+      <div>
+        <img
+          alt="avatar"
+          width="40px"
+          height="auto"
+          src="https://ik.imagekit.io/s2fpg15d4rx/kisspng-computer-icons-portable-network-graphics-user-icon-my-b-tab-svg-png-icon-free-download-8-584-onl-5bfa37607f3959.2067798415431248325211_-Xj5AVLFlyoz.png"
+        />
+      </div>
+
       <p className="name" id="name_user">
         Wellcome {name ? name : 'Guest'}
       </p>
-      <button onClick={onLogout}>
-        Log off
-        <img
-          alt="logout"
-          src="https://ik.imagekit.io/s2fpg15d4rx/imgbin_web-hosting-icon-logout-icon-arrow-icon-png_8SQmsSuCZ.png"
-          width="50px"
-        />
+      <button onClick={onLogout} className="btn_logout">
+        <ExitToAppIcon fontSize="large" />
       </button>
     </div>
   );
